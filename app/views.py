00f8f5dd2576719c5b8687b80800a02d924b6532
Dev-magno <magnolopes1212@gmail.com>
2024-11-26 09:45:20 -0300

    Projeto Flask

---
+++ app/views.py
@@ -0,0 +1,40 @@
+from app import app, db
+from flask import render_template, url_for, request, redirect
+from app.models import Contato
+from app.forms import ContatoForm
+
+@app.route('/')
+def homepage():
+    usuario = 'Magno'
+    idade = 43
+    profissao = 'Dsenvolvedor'
+    context = {
+        'usuario': usuario,
+        'idade': idade,
+        'profissao': profissao
+    }
+    return render_template('index.html', context=context)
+
+
+@app.route('/contato/', methods = ['GET', 'POST'])
+def contato():
+    form = ContatoForm()
+    context = {}
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('homepage'))
+     
+    return render_template('contato.html', context=context, form=form)
+
+
+@app.route('/contato/lista')
+def contatoLista():
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa', '')
+
+    dados = Contato.query.order_by('nome')
+    if pesquisa != '':
+        dados = dados.filter_by(nome=pesquisa)
+    context = {'dados': dados.all()}
+
+    return render_template('contato_lista.html', context=context)

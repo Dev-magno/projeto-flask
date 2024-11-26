02b9d6447394305adabd505967fed045946ef16b
Dev-magno <magnolopes1212@gmail.com>
2024-11-26 11:14:18 -0300

    Atualizações no contato e add chave secreta

--- app/views.py
+++ app/views.py
@@ -38,3 +38,8 @@ def contatoLista():
     context = {'dados': dados.all()}
 
     return render_template('contato_lista.html', context=context)
+
+@app.route('/contato/<int:id>/')
+def contatoDatail(id):
+    obj = Contato.query.get(id)
+    return render_template('contato_datail.html', obj=obj)
